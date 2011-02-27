e27c7a761d59396e6f0d3027b94aa2027f842b87
tk0miya <i.tkomiya@gmail.com>
2011-02-28 01:04:34 +0900

    * Implement QB renderer (for SVG only)

--- blockdiagcontrib/qb.py
+++ blockdiagcontrib/qb.py
@@ -1,40 +1,57 @@
 # -*- coding: utf-8 -*-
-from blockdiag.noderenderer import NodeShape
+from blockdiag.noderenderer.box import Box
 from blockdiag.noderenderer import install_renderer
 from blockdiag.utils.XY import XY
+from blockdiag.SVGdraw import pathdata
 
 
-class Square(NodeShape):
-    def __init__(self, node, metrix=None):
-        super(Square, self).__init__(node, metrix)
-
-        r = min(metrix.nodeWidth, metrix.nodeHeight) / 2 + metrix.cellSize / 2
-        pt = metrix.cell(node).center()
-        self.connectors = [XY(pt.x, pt.y - r),  # top
-                           XY(pt.x + r, pt.y),  # right
-                           XY(pt.x, pt.y + r),  # bottom
-                           XY(pt.x - r, pt.y)]  # left
-        self.textbox = (pt.x - r, pt.y - r, pt.x + r, pt.y + r)
-
+class QB(Box):
     def render_shape(self, drawer, format, **kwargs):
+        super(QB, self).render_shape(drawer, format, **kwargs)
+
         outline = kwargs.get('outline')
         fill = kwargs.get('fill')
 
         # draw outline
-        if kwargs.get('shadow'):
-            box = self.shift_shadow(self.textbox)
-            drawer.rectangle(box, fill=fill, outline=fill,
-                           filter='transp-blur')
-        elif self.node.background:
-            drawer.rectangle(self.textbox, fill=self.node.color,
-                             outline=self.node.color)
-            drawer.loadImage(self.node.background, self.textbox)
-            drawer.rectangle(self.textbox, fill="none", outline=outline,
-                             style=self.node.style)
-        else:
-            drawer.rectangle(self.textbox, fill=self.node.color,
-                             outline=outline, style=self.node.style)
+        if not kwargs.get('shadow'):
+            m = self.metrix
+            h = m.nodeHeight / 4
+            w = m.nodeWidth / 6
+            r = m.cellSize
+
+            # left eye
+            xy = self.metrix.cell(self.node).top()
+            left = XY(xy.x - w, xy.y + h)
+            self.render_eye(drawer, left)
+
+            # right eye
+            right = XY(xy.x + w, xy.y + h)
+            self.render_eye(drawer, right)
+
+            # left mouthline
+            xy = self.metrix.cell(self.node).bottom()
+            y = xy.y - h * 3 / 2 + r / 2
+
+            path = pathdata(xy.x - r * 3, y)
+            path.ellarc(r * 3 / 2, h * 3 / 4, 0, 0, 0, xy.x, y)
+            path.ellarc(r * 3 / 2, h * 3 / 4, 0, 0, 0, xy.x + r * 3, y)
+            drawer.path(path, fill="none", outline=fill)
+
+    def render_eye(self, drawer, center):
+        m = self.metrix
+
+        r = m.cellSize
+        eye = (center.x - r, center.y - r, center.x + r, center.y + r)
+        drawer.ellipse(eye, fill='orange', outline='orange')
+
+        r = m.cellSize * 2 / 3
+        eye = (center.x - r, center.y - r, center.x + r, center.y + r)
+        drawer.ellipse(eye, fill='red', outline='red')
+
+        r = m.cellSize / 3
+        eye = (center.x + r, center.y - r * 3, center.x + r * 3, center.y - r)
+        drawer.ellipse(eye, fill='white', outline='white')
 
 
 def setup(self):
-    install_renderer('square', Square)
+    install_renderer('qb', QB)
