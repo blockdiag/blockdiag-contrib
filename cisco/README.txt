985340f29fe232989998e3cd1bf62b6be5aa2620
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-06-10 00:03:23 +0900

    * Implement blockdiagcontrib-cisco first version

---
+++ cisco/README.txt
@@ -0,0 +1,21 @@
+A plugin for `blockdiag` that provides node-renderers for networking
+
+Diagram examples
+================
+`blockdiagcontrib-cisco` renders network icon as node's shape.
+
+Example::
+
+    diagram admin {
+      A [shape = "cisco.router"];
+    }
+
+
+Requirements
+============
+* blockdiag 0.8.0 or later
+
+
+License
+=======
+Python Software Foundation License.
---
+++ cisco/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os, sys
+
+version = '0.1.0'
+long_description = open("README.txt").read()
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Python Software Foundation License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup",
+]
+
+setup(
+     name='blockdiagcontrib-cisco',
+     version=version,
+     description='noderenderer plugin for blockdiag',
+     long_description=long_description,
+     classifiers=classifiers,
+     keywords=['diagram','generator'],
+     author='Takeshi Komiya',
+     author_email='i.tkomiya at gmail.com',
+     url='http://tk0miya.bitbucket.org/blockdiag/build/html/index.html',
+     license='PSL',
+     packages=find_packages(),
+     package_data = {'': ['buildout.cfg']},
+     namespace_packages=['blockdiagcontrib'],
+     include_package_data=True,
+     install_requires=[
+        'blockdiag',
+        'setuptools',
+     ],
+     entry_points="""
+        [blockdiag_noderenderer]
+        cisco = blockdiagcontrib.cisco
+     """,
+)
+
