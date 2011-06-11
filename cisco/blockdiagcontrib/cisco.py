5229f25d6adb5505d4867dedba2a34d947d8493b
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-06-11 16:48:53 +0900

    * Add jpg file header parser

--- cisco/blockdiagcontrib/cisco.py
+++ cisco/blockdiagcontrib/cisco.py
@@ -56,9 +56,14 @@ def gen_image_class(image_path):
 
         @property
         def image_size(self):
-            import PIL.Image
+            try:
+                import PIL.Image
+                raise ImportError()
 
-            size = PIL.Image.open(image_path).size
+                size = PIL.Image.open(image_path).size
+            except ImportError:
+                size = JpegFile.get_size(image_path)
+
             box = [self.metrix.nodeWidth, self.metrix.nodeHeight]
 
             if box[0] < size[0] or box[1] < size[1]:
@@ -74,6 +79,77 @@ def gen_image_class(image_path):
                 drawer.loadImage(self.image_path, self.image_box)
 
     return CiscoImage
+
+# -*- coding: utf-8 -*-
+
+class StreamReader(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.pos = 0
+
+    def read_byte(self):
+        byte = self.stream[self.pos]
+        self.pos += 1
+        return ord(byte)
+
+    def read_word(self):
+        byte1, byte2 = self.stream[self.pos:self.pos + 2]
+        self.pos += 2
+        return (ord(byte1)<<8) + ord(byte2)
+
+    def read_bytes(self, n):
+        bytes = self.stream[self.pos:self.pos + n]
+        self.pos += n
+        return bytes
+
+class JpegHeaderReader(StreamReader):
+    M_SOI   = 0xd8
+    M_SOS   = 0xda
+
+    def read_marker(self):
+        if self.read_byte() != 255:
+            raise ValueError("error reading marker")
+        return self.read_byte()
+
+    def skip_marker(self):
+        """Skip over an unknown or uninteresting variable-length marker"""
+        length = self.read_word()
+        self.read_bytes(length - 2)
+
+    def __iter__(self):
+      while True:
+          if self.read_byte() != 255:
+              raise ValueError("error reading marker")
+
+          marker = self.read_byte()
+          if marker == self.M_SOI:
+              length = 0
+              data = ''
+          else:
+              length = self.read_word()
+              data = self.read_bytes(length - 2)
+
+          yield (marker, data)
+
+          if marker == self.M_SOS:
+              raise StopIteration()
+
+
+class JpegFile(object):
+    M_SOF0  = 0xc0
+    M_SOF1  = 0xc1
+
+    @classmethod
+    def get_size(self, filename):
+        image = open(filename, 'rb').read()
+        headers = JpegHeaderReader(image)
+        for header in headers:
+            if header[0] in (self.M_SOF0, self.M_SOF1):
+                data = header[1]
+
+                height = (ord(data[1])<<8) + ord(data[2])
+                width = (ord(data[3])<<8) + ord(data[4])
+                return (width, height)
 
 def to_classname(filename):
     filename = re.sub('\.[a-z]+$', '', filename)
