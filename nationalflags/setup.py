e860fbf11c30ee9b0d92e07be8b221174dc40320
tk0miya <i.tkomiya@gmail.com>
2014-06-12 00:54:25 +0900

    nationalflags: Fix flake8 violations

--- nationalflags/setup.py
+++ nationalflags/setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import os, sys
 
 version = '0.1.5'
 long_description = open("README.txt").read()
@@ -16,27 +15,27 @@ classifiers = [
 ]
 
 setup(
-     name='blockdiagcontrib-nationalflags',
-     version=version,
-     description='noderenderer plugin for blockdiag',
-     long_description=long_description,
-     classifiers=classifiers,
-     keywords=['diagram','generator'],
-     author='Takeshi Komiya',
-     author_email='i.tkomiya at gmail.com',
-     url='http://tk0miya.bitbucket.org/blockdiag/build/html/index.html',
-     license='PSL',
-     packages=find_packages(),
-     package_data = {'': ['buildout.cfg']},
-     namespace_packages=['blockdiagcontrib'],
-     include_package_data=True,
-     install_requires=[
+    name='blockdiagcontrib-nationalflags',
+    version=version,
+    description='noderenderer plugin for blockdiag',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['diagram', 'generator'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://tk0miya.bitbucket.org/blockdiag/build/html/index.html',
+    license='PSL',
+    packages=find_packages(),
+    package_data={'': ['buildout.cfg']},
+    namespace_packages=['blockdiagcontrib'],
+    include_package_data=True,
+    install_requires=[
         'blockdiag>=1.2.0',
         'setuptools',
-     ],
-     entry_points="""
-        [blockdiag_noderenderer]
-        nationalflags = blockdiagcontrib.nationalflags
-     """,
+    ],
+    entry_points="""
+       [blockdiag_noderenderer]
+       nationalflags = blockdiagcontrib.nationalflags
+    """,
 )
 
