aba6b98ed4b370c081f0cf545fd2652a09d04224
tk0miya <i.tkomiya@gmail.com>
2014-07-19 15:53:54 +0900

    math: Bump vesion

--- math/setup.py
+++ math/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.1.1'
 requires = ['blockdiag >= 1.4.2']
 classifiers = [
     "Development Status :: 4 - Beta",
